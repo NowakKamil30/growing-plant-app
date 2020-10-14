f40f0ec3dbdf20317cc55524d1d9b80bb6e50727
Kamil Nowak <kamilnowakx98@gmail.com>
2020-10-14 13:22:37 +0200

    init backend app

---
+++ backend/growing-plant-app/src/main/java/com/growingplantapp/GrowingPlantAppApplication.java
@@ -0,0 +1,12 @@
+package com.growingplantapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowingPlantAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GrowingPlantAppApplication.class, args);
+    }
+
+}
