7f89ab86889d43c1f8ad2acb549414670204eb5d
Kamil Nowak <kamilnowakx98@gmail.com>
2020-11-02 00:25:27 +0100

    add entity GrowingCondition

---
+++ backend/growing-plant-app/src/main/java/com/growingplantapp/entities/GrowingCondition.java
@@ -0,0 +1,63 @@
+package com.growingplantapp.entities;
+
+import lombok.Builder;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "Growing_conditions")
+public class GrowingCondition {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private int irrigation;
+    private int insolation;
+    @Column(name = "time_of_growth")
+    private int timeOfGrowth;
+    @NotEmpty
+    private String color;
+
+    public GrowingCondition() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getIrrigation() {
+        return irrigation;
+    }
+
+    public void setIrrigation(int irrigation) {
+        this.irrigation = irrigation;
+    }
+
+    public int getInsolation() {
+        return insolation;
+    }
+
+    public void setInsolation(int insolation) {
+        this.insolation = insolation;
+    }
+
+    public int getTimeOfGrowth() {
+        return timeOfGrowth;
+    }
+
+    public void setTimeOfGrowth(int timeOfGrowth) {
+        this.timeOfGrowth = timeOfGrowth;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
