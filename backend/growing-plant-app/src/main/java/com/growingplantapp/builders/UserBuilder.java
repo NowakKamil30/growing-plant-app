cb862497dc2620aa1d3ae3a1f5e49147c92765a0
Kamil Nowak <kamilnowakx98@gmail.com>
2020-11-02 00:27:41 +0100

    add UserBuilder

---
+++ backend/growing-plant-app/src/main/java/com/growingplantapp/builders/UserBuilder.java
@@ -0,0 +1,64 @@
+package com.growingplantapp.builders;
+
+import com.growingplantapp.entities.Device;
+import com.growingplantapp.entities.Role;
+import com.growingplantapp.entities.User;
+
+import java.util.List;
+
+public final class UserBuilder {
+    private Long id;
+    private String email;
+    private String name;
+    private Role role;
+    private List<Device> devices;
+    private boolean isActive;
+
+    private UserBuilder() {
+    }
+
+    public static UserBuilder anUser() {
+        return new UserBuilder();
+    }
+
+    public UserBuilder withId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public UserBuilder withIsActive(boolean isActive) {
+        this.isActive = isActive;
+        return this;
+    }
+
+    public UserBuilder withEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public UserBuilder withName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public UserBuilder withRole(Role role) {
+        this.role = role;
+        return this;
+    }
+
+    public UserBuilder withDevices(List<Device> devices) {
+        this.devices = devices;
+        return this;
+    }
+
+    public User build() {
+        User user = new User();
+        user.setId(id);
+        user.setEmail(email);
+        user.setName(name);
+        user.setRole(role);
+        user.setDevices(devices);
+        user.setActive(isActive);
+        return user;
+    }
+}
