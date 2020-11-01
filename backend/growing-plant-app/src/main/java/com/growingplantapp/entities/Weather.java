0e971cfe0b9dfeccdbc49f85a189d8c6c72de47c
Kamil Nowak <kamilnowakx98@gmail.com>
2020-11-02 00:21:09 +0100

    add weather entity

---
+++ backend/growing-plant-app/src/main/java/com/growingplantapp/entities/Weather.java
@@ -0,0 +1,64 @@
+package com.growingplantapp.entities;
+
+import lombok.Builder;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Weathers")
+public class Weather {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private int temperature;
+    private int rain;
+    private int insolation;
+    @NotNull
+    private LocalDateTime localDateTime;
+
+
+    public Weather() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+    }
+
+    public int getRain() {
+        return rain;
+    }
+
+    public void setRain(int rain) {
+        this.rain = rain;
+    }
+
+    public int getInsolation() {
+        return insolation;
+    }
+
+    public void setInsolation(int insolation) {
+        this.insolation = insolation;
+    }
+
+    public LocalDateTime getLocalDateTime() {
+        return localDateTime;
+    }
+
+    public void setLocalDateTime(LocalDateTime localDateTime) {
+        this.localDateTime = localDateTime;
+    }
+}
