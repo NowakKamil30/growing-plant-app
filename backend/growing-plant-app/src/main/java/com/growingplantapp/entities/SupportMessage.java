b1aa1790b52ad221ac69a58f1e2eb1d61e3aea04
Kamil Nowak <kamilnowakx98@gmail.com>
2020-11-02 00:23:08 +0100

    add entity SupportMessage

---
+++ backend/growing-plant-app/src/main/java/com/growingplantapp/entities/SupportMessage.java
@@ -0,0 +1,45 @@
+package com.growingplantapp.entities;
+
+import lombok.Builder;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "Support_messages")
+public class SupportMessage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    @NotEmpty
+    private String message;
+    @ManyToOne
+    private User user;
+
+    public SupportMessage() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
