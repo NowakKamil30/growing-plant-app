b303774fab63b288ec7fb230bb8e92e1a7929ef7
Kamil Nowak <kamilnowakx98@gmail.com>
2020-11-02 00:24:51 +0100

    add entity plant

---
+++ backend/growing-plant-app/src/main/java/com/growingplantapp/entities/Plant.java
@@ -0,0 +1,55 @@
+package com.growingplantapp.entities;
+
+import lombok.Builder;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "Plants")
+public class Plant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    @NotEmpty
+    private String name;
+    @ManyToOne
+    private Device device;
+    @OneToOne
+    private GrowingCondition growingCondition;
+
+    public Plant() {
+    }
+
+    public GrowingCondition getGrowingCondition() {
+        return growingCondition;
+    }
+
+    public void setGrowingCondition(GrowingCondition growingCondition) {
+        this.growingCondition = growingCondition;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Device getDevice() {
+        return device;
+    }
+
+    public void setDevice(Device device) {
+        this.device = device;
+    }
+}
