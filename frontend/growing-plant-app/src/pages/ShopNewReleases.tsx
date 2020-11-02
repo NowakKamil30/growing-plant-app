9db98599b3dfbb8a10c59e5d67ed41c2e126c623
Kamil Nowak <kamilnowakx98@gmail.com>
2020-11-02 13:37:50 +0100

    add ShopNewReleases page

---
+++ frontend/growing-plant-app/src/pages/ShopNewReleases.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Trans } from 'react-i18next';
+
+const ShopNewReleases = () => {
+    return (
+        <h1><Trans i18nKey='shop.NewReleases'/></h1>
+    );
+};
+
+export default ShopNewReleases;
