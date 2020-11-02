180c73cb9da814752afe92886c9e06782b745720
Kamil Nowak <kamilnowakx98@gmail.com>
2020-11-02 13:38:43 +0100

    add MyAccount page

---
+++ frontend/growing-plant-app/src/pages/MyAccount.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Trans } from 'react-i18next';
+
+const MyAccount = () => {
+    return (
+        <h1><Trans i18nKey='pages.myAccount'/></h1>
+    );
+};
+
+export default MyAccount;
