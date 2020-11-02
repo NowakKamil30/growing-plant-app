f13cf384b849ee1eb1e96e3f7671f6e0246453b3
Kamil Nowak <kamilnowakx98@gmail.com>
2020-11-02 13:41:48 +0100

    add ShopBasket page

---
+++ frontend/growing-plant-app/src/pages/ShopBasket.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Trans } from 'react-i18next';
+
+const ShopBasket = () => {
+    return (
+        <h1><Trans i18nKey='shop.basket'/></h1>
+    );
+};
+
+export default ShopBasket;
