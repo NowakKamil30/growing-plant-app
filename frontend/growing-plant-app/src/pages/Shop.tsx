be4319c6fb2dbbae9c9895343e6c83b4234d1f76
Kamil Nowak <kamilnowakx98@gmail.com>
2020-11-02 13:37:23 +0100

    add shop page

---
+++ frontend/growing-plant-app/src/pages/Shop.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Trans } from 'react-i18next';
+
+const Shop = () => {
+    return (
+        <h1><Trans i18nKey='pages.shop'/></h1>
+    );
+};
+
+export default Shop;
