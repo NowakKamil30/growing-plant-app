768d0dae7507e9636d76fdf16533461382d65301
Kamil Nowak <kamilnowakx98@gmail.com>
2020-11-02 14:53:34 +0100

    add security paths

--- frontend/growing-plant-app/src/router/RouterSwitch.tsx
+++ frontend/growing-plant-app/src/router/RouterSwitch.tsx
@@ -1,32 +1,51 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
+import { connect, ConnectedProps } from 'react-redux';
 import {
     Route,
     Switch
   } from 'react-router-dom';
+import { Role } from '../enums/Role';
 import About from '../pages/About';
 import AdminPanel from '../pages/AdminPanel';
 import Error404 from '../pages/Error404';
 import { HomePage } from '../pages/HomePage';
 import MyAccount from '../pages/MyAccount';
 import MyDevice from '../pages/MyDevice';
+import { ReduceTypes } from '../stores/reducers';
 import ContactRouter from './ContactRouter';
 import PrivateRoute from './PrivateRoute';
 import ShopRouter from './ShopRouter';
-const RouterSwitch = () => (
+
+interface MapStateToProps {
+    role: Role;
+}
+
+const MapStateToProps = (state: ReduceTypes): MapStateToProps => ({
+    role: state.auth.role
+});
+
+const connector = connect(MapStateToProps, null);
+
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+const RouterSwitch: React.FC<PropsFromRedux> = ({
+    role
+}) => (
     <Switch>
         <Route exact path='/'>
             <HomePage/>
         </Route>
         <PrivateRoute
         path='/my-account'
-        isAuth={ false }
+        isAuth={ role === Role.ADMIN || role === Role.USER }
         redirectPath='/'
         >
             <MyAccount/>
         </PrivateRoute>
         <PrivateRoute
         path='/my-device/:id'
-        isAuth={ false }
+        isAuth={ role === Role.ADMIN || role === Role.USER }
         redirectPath='/'
         >
             <MyDevice/>
@@ -42,7 +61,7 @@ const RouterSwitch = () => (
         </Route>
         <PrivateRoute
         path='/admin-panel'
-        isAuth={ false }
+        isAuth={ role === Role.ADMIN }
         redirectPath='/'
         >
             <AdminPanel/>
@@ -53,4 +72,4 @@ const RouterSwitch = () => (
     </Switch>
 );
 
-export default RouterSwitch;
+export default connector(RouterSwitch);
