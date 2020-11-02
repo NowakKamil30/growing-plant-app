038643dc42fed07fb62fdc7e74646e83543bac2a
Kamil Nowak <kamilnowakx98@gmail.com>
2020-11-02 13:40:00 +0100

    add ContactFaQ page

---
+++ frontend/growing-plant-app/src/pages/ContactFaQ.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Trans } from 'react-i18next';
+
+const ContactFaQ = () => {
+    return (
+        <h1><Trans i18nKey='contact.faq'/></h1>
+    );
+};
+
+export default ContactFaQ;
