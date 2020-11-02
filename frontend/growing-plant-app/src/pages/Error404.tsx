c929f95686bea3888d5bea41f50f3fbbb3718717
Kamil Nowak <kamilnowakx98@gmail.com>
2020-11-02 13:38:09 +0100

    add Error404 page

---
+++ frontend/growing-plant-app/src/pages/Error404.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Trans } from 'react-i18next';
+
+const Error404 = () => {
+    return (
+        <h1><Trans i18nKey='pages.error404'/></h1>
+    );
+};
+
+export default Error404;
