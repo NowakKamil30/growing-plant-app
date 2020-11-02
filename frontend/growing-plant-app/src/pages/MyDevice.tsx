1f0bf16b2117bd90a916162592486a12424a9e44
Kamil Nowak <kamilnowakx98@gmail.com>
2020-11-02 13:41:28 +0100

    add MyDevice page

---
+++ frontend/growing-plant-app/src/pages/MyDevice.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Trans } from 'react-i18next';
+
+const MyDevice = () => {
+    return (
+        <h1><Trans i18nKey='pages.myDevices'/></h1>
+    );
+};
+
+export default MyDevice;
