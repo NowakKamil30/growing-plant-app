81118da1962554af5d467dc46d6d177bbc5b3c7f
Kamil Nowak <kamilnowakx98@gmail.com>
2020-11-02 01:22:25 +0100

    add PlantDTO

--- backend/growing-plant-app/src/main/java/com/growingplantapp/DTOs/PlantDTO.java
+++ backend/growing-plant-app/src/main/java/com/growingplantapp/DTOs/PlantDTO.java
@@ -1,2 +1,43 @@
-package com.growingplantapp.DTOs;public class PlantDTO {
+package com.growingplantapp.DTOs;
+
+public class PlantDTO {
+    private Long id;
+    private String name;
+    private Long deviceId;
+    private Long growingConditionId;
+
+    public PlantDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(Long deviceId) {
+        this.deviceId = deviceId;
+    }
+
+    public Long getGrowingConditionId() {
+        return growingConditionId;
+    }
+
+    public void setGrowingConditionId(Long growingConditionId) {
+        this.growingConditionId = growingConditionId;
+    }
 }
