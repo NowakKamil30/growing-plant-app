fc2281c08e22fc27f2ea340e9b668a767b1abf94
Kamil Nowak <kamilnowakx98@gmail.com>
2020-11-02 13:39:22 +0100

    add AdminPanel page

---
+++ frontend/growing-plant-app/src/pages/AdminPanel.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Trans } from 'react-i18next';
+
+const AdminPanel = () => {
+    return (
+        <h1><Trans i18nKey='pages.adminPanel'/></h1>
+    );
+};
+
+export default AdminPanel;
