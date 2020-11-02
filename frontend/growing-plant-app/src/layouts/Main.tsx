32d8545861c7694d05ac0848e8e1cb0ef5f4c4b4
Kamil Nowak <kamilnowakx98@gmail.com>
2020-11-02 11:18:51 +0100

    add Main

---
+++ frontend/growing-plant-app/src/layouts/Main.tsx
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Box, createStyles, makeStyles, Theme } from '@material-ui/core';
+import React from 'react';
+import RouterSwitch from '../router/RouterSwitch';
+
+const Main = () => {
+    const { root } = useStyles();
+
+    return (
+        <Box
+        component='main'
+        className={ root }
+        >
+             <RouterSwitch/>
+        </Box>
+    );
+};
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+        flexGrow: 2
+    },
+  }),
+);
+
+export default Main;
