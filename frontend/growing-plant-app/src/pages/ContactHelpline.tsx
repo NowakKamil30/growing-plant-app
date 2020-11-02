68959c1911a528a56ce1b3576b44392a9b16b37f
Kamil Nowak <kamilnowakx98@gmail.com>
2020-11-02 13:40:21 +0100

    add ContactHelpline page

---
+++ frontend/growing-plant-app/src/pages/ContactHelpline.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Trans } from 'react-i18next';
+
+const ContactHelpline = () => {
+    return (
+        <h1><Trans i18nKey='contact.helpline'/></h1>
+    );
+};
+
+export default ContactHelpline;
