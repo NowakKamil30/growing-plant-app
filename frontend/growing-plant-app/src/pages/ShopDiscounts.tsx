b3edb32d3bc657a97e35c2e2cbdd963fe044e67b
Kamil Nowak <kamilnowakx98@gmail.com>
2020-11-02 13:42:07 +0100

    add ShopDiscount page

---
+++ frontend/growing-plant-app/src/pages/ShopDiscounts.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Trans } from 'react-i18next';
+
+const ShopDiscounts = () => {
+    return (
+        <h1><Trans i18nKey='shop.discounts'/></h1>
+    );
+};
+
+export default ShopDiscounts;
