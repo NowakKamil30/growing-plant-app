ed09ce4a493c487de88c11d73f85a7c77bf236f5
Kamil Nowak <kamilnowakx98@gmail.com>
2020-11-02 13:40:57 +0100

    add ContactMessage page

---
+++ frontend/growing-plant-app/src/pages/ContactMessage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Trans } from 'react-i18next';
+
+const ContactMessage = () => {
+    return (
+        <h1><Trans i18nKey='contact.message'/></h1>
+    );
+};
+
+export default ContactMessage;
