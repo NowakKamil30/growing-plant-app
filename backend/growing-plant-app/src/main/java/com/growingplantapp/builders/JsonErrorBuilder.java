2231f82a1b129cb0f2966002db4f6aa2cfe09173
Kamil Nowak <kamilnowakx98@gmail.com>
2020-11-02 01:19:04 +0100

    add JsonErrorBuilder

--- backend/growing-plant-app/src/main/java/com/growingplantapp/builders/JsonErrorBuilder.java
+++ backend/growing-plant-app/src/main/java/com/growingplantapp/builders/JsonErrorBuilder.java
@@ -1,2 +1,32 @@
-package com.growingplantapp.builders;public class JsonErrorBuilder {
+package com.growingplantapp.builders;
+
+import com.growingplantapp.JsonError;
+
+public final class JsonErrorBuilder {
+    private String message;
+    private String field;
+
+    private JsonErrorBuilder() {
+    }
+
+    public static JsonErrorBuilder aJsonError() {
+        return new JsonErrorBuilder();
+    }
+
+    public JsonErrorBuilder withMessage(String message) {
+        this.message = message;
+        return this;
+    }
+
+    public JsonErrorBuilder withField(String field) {
+        this.field = field;
+        return this;
+    }
+
+    public JsonError build() {
+        JsonError jsonError = new JsonError();
+        jsonError.setMessage(message);
+        jsonError.setField(field);
+        return jsonError;
+    }
 }
