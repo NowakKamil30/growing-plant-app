ceb9dd67b3eba9b3ef4b05853b6c05134b33cd90
Kamil Nowak <kamilnowakx98@gmail.com>
2020-11-02 13:39:04 +0100

    add About page

---
+++ frontend/growing-plant-app/src/pages/About.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Trans } from 'react-i18next';
+
+const About = () => {
+    return (
+        <h1><Trans i18nKey='pages.about'/></h1>
+    );
+};
+
+export default About;
