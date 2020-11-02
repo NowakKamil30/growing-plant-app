9407556e71a73266336d83d13191156edf98d952
Kamil Nowak <kamilnowakx98@gmail.com>
2020-11-02 01:19:40 +0100

    edit ExpectionHandlerController

--- backend/growing-plant-app/src/main/java/com/growingplantapp/controllers/ExceptionHandlerController.java
+++ backend/growing-plant-app/src/main/java/com/growingplantapp/controllers/ExceptionHandlerController.java
@@ -1,6 +1,7 @@
 package com.growingplantapp.controllers;
 
 import com.growingplantapp.JsonError;
+import com.growingplantapp.builders.JsonErrorBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,9 +13,8 @@ public class ExceptionHandlerController {
 
     @ExceptionHandler(NumberFormatException.class)
     public ResponseEntity<List<JsonError>> numberFormatHandler(NumberFormatException numberFormatException) {
-        List<JsonError> jsonErrorList = List.of(JsonError
-                .builder()
-                .message(numberFormatException.getMessage())
+        List<JsonError> jsonErrorList = List.of(JsonErrorBuilder.aJsonError()
+                .withMessage(numberFormatException.getMessage())
                 .build());
         return ResponseEntity.badRequest().body(jsonErrorList);
     }
