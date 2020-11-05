096e6332a7acfcfe9df5ad48117ba87aec5af137
Kamil Nowak <kamilnowakx98@gmail.com>
2020-11-05 01:18:48 +0100

    add BadJwtException

--- backend/growing-plant-app/src/main/java/com/growingplantapp/exceptions/BadJwtException.java
+++ backend/growing-plant-app/src/main/java/com/growingplantapp/exceptions/BadJwtException.java
@@ -1,2 +1,14 @@
-package com.growingplantapp.exceptions;public class BadJwtException {
+package com.growingplantapp.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.FORBIDDEN)
+public class BadJwtException extends RuntimeException {
+    public BadJwtException() {
+    }
+
+    public BadJwtException(String message) {
+        super(message);
+    }
 }
