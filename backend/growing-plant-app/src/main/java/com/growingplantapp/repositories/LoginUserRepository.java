896860c395c47a349da1974c6507cf2ffda65541
Kamil Nowak <kamilnowakx98@gmail.com>
2020-11-05 01:22:04 +0100

    add LoginUserRepository

--- backend/growing-plant-app/src/main/java/com/growingplantapp/repositories/LoginUserRepository.java
+++ backend/growing-plant-app/src/main/java/com/growingplantapp/repositories/LoginUserRepository.java
@@ -1,2 +1,10 @@
-package com.growingplantapp.repositories;public interface LoginUserRepository {
+package com.growingplantapp.repositories;
+
+import com.growingplantapp.entities.LoginUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LoginUserRepository extends JpaRepository<LoginUser, Long> {
+    LoginUser findByUsername(String username);
 }
