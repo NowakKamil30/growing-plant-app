b7131380a6f7e2c71e2d02ad707526ed6294a629
Kamil Nowak <kamilnowakx98@gmail.com>
2020-11-05 01:19:31 +0100

    add AuthController

--- backend/growing-plant-app/src/main/java/com/growingplantapp/controllers/AuthController.java
+++ backend/growing-plant-app/src/main/java/com/growingplantapp/controllers/AuthController.java
@@ -1,2 +1,45 @@
-package com.growingplantapp.controllers;public class AuthController {
+package com.growingplantapp.controllers;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.growingplantapp.entities.LoginUser;
+import com.growingplantapp.models.LoginResponse;
+import com.growingplantapp.services.LoginUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+
+    private final LoginUserService loginUserService;
+
+    @Autowired
+    public AuthController(LoginUserService loginUserService) {
+        this.loginUserService = loginUserService;
+    }
+
+    @PostMapping
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginUser user) {
+        String sign = null;
+        LoginUser loginUser = loginUserService.loadUserByUsername(user.getUsername());
+        if (loginUser != null && BCrypt.checkpw(user.getPassword(), loginUser.getPassword())) {
+            sign = JWT.create()
+                    .withClaim("name", loginUser.getUsername())
+                    .withClaim("role", "ROLE_" + loginUser.getRole().toString())
+                    .sign(Algorithm.HMAC512("McQfTjWnZr4u7x!A%D*G-KaNdRgUkXp2s5v8y/B?E(H+MbQeShVmYq3t6w9z$C&F"));
+        } else {
+            return ResponseEntity.noContent().build();
+        }
+        LoginResponse loginResponse = new LoginResponse();
+        loginResponse.setToken(sign);
+        loginResponse.setRole(loginUser.getRole());
+        loginResponse.setUserId(loginUser.getUser().getId());
+        return ResponseEntity.ok(loginResponse);
+    }
 }
