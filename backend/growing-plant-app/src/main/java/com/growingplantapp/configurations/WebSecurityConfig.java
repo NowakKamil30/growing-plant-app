c3a3860b15ef7d2d873de37175c83c53bcead453
Kamil Nowak <kamilnowakx98@gmail.com>
2020-11-05 01:20:08 +0100

    add WebSecurityConfig

--- backend/growing-plant-app/src/main/java/com/growingplantapp/configurations/WebSecurityConfig.java
+++ backend/growing-plant-app/src/main/java/com/growingplantapp/configurations/WebSecurityConfig.java
@@ -1,2 +1,46 @@
-package com.growingplantapp.configurations;public class WebSecurityConfig {
+package com.growingplantapp.configurations;
+
+import com.growingplantapp.filters.JwtFilter;
+import com.growingplantapp.services.LoginUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final LoginUserService loginUserService;
+
+    @Autowired
+    public WebSecurityConfig(LoginUserService loginUserService) {
+        this.loginUserService = loginUserService;
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(loginUserService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/api/v1/auth/**").permitAll()
+                .and()
+                .addFilterBefore(new JwtFilter(), UsernamePasswordAuthenticationFilter.class)
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/api/v1/user/**").authenticated();
+        http.csrf().disable();
+    }
 }
