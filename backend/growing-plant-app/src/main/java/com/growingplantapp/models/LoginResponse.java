ab6a9f1b0c57a268b90acafbd0a76c442ba4857a
Kamil Nowak <kamilnowakx98@gmail.com>
2020-11-05 01:21:36 +0100

    add LoginResponse

--- backend/growing-plant-app/src/main/java/com/growingplantapp/models/LoginResponse.java
+++ backend/growing-plant-app/src/main/java/com/growingplantapp/models/LoginResponse.java
@@ -1,2 +1,36 @@
-package com.growingplantapp.models;public class LoginRespons {
+package com.growingplantapp.models;
+
+import com.growingplantapp.entities.Role;
+
+public class LoginResponse {
+    private String token;
+    private Role role;
+    private Long userId;
+
+    public LoginResponse() {
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
 }
