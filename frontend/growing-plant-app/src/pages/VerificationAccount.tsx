21af0f96172763c1fc610a46b05298dc27147686
Kamil Nowak <kamilnowakx98@gmail.com>
2020-11-18 22:18:45 +0100

    add VerificationAccount

---
+++ frontend/growing-plant-app/src/pages/VerificationAccount.tsx
@@ -0,0 +1,16 @@
+import { Box } from '@material-ui/core';
+import React from 'react';
+import { Trans } from 'react-i18next';
+
+const VerificationAccount = (props): JSX.Element => {
+    console.log(props);
+
+    return (
+        <Box component='h1'>
+            <Trans i18nKey=''></Trans>
+        </Box>
+    );
+};
+
+
+export default VerificationAccount;
